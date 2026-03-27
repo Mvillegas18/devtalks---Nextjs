7cc568da52769362f743a16142292f1fa167711f
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-03-27 15:09:36 -0500

    Add news images in public and route public page.tsx

---
+++ app/(public)/page.tsx
@@ -0,0 +1,15 @@
+import { type Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'Inicio - DevTalks',
+	description:
+		'Bienvenido a DevTalks, tu espacio para compartir conocimientos y experiencias en el mundo del desarrollo de software. Únete a nuestra comunidad y participa en discusiones, eventos y recursos para desarrolladores de todos los niveles.',
+}
+
+export default function Home() {
+	return (
+		<>
+			<h1>Inicio</h1>
+		</>
+	)
+}
