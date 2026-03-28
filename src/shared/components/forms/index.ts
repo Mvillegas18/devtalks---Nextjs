f0b70004b7c0fd8f4ccb4141424fd2015b80694a
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-03-28 14:27:12 -0500

    add form components index file to export Form, FormInput, FormLabel, and FormSubmit

---
+++ src/shared/components/forms/index.ts
@@ -0,0 +1,6 @@
+import { FormInput } from './FormInput'
+import { FormLabel } from './FormLabel'
+import { FormSubmit } from './FormSubmit'
+import { Form } from './Form'
+
+export { FormInput, FormLabel, FormSubmit, Form }
