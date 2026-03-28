1b7906eab326ebc131f17d9e4b8ade262459eae6
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-03-28 18:37:57 -0500

    add validation schemas for user authentication using zod

---
+++ src/features/auth/shemas/authSchema.ts
@@ -0,0 +1,25 @@
+import z from 'zod'
+
+export const BaseAuthSchema = z.object({
+	name: z.string().trim().min(1, { error: 'El nombre es requerido' }),
+	email: z.email({ error: 'El email no es válido' }),
+	password: z
+		.string()
+		.trim()
+		.min(8, { error: 'La contraseña debe tener al menos 8 caracteres' }),
+	passwordConfirmation: z.string().trim().min(1, {
+		error: 'La confirmación de contraseña es requerida',
+	}),
+})
+
+export const SignUpSchema = BaseAuthSchema.pick({
+	name: true,
+	email: true,
+	password: true,
+	passwordConfirmation: true,
+}).refine((data) => data.password === data.passwordConfirmation, {
+	error: 'Las contraseñas no coinciden',
+	path: ['passwordConfirmation'],
+})
+
+export type SignUpInput = z.infer<typeof SignUpSchema>
