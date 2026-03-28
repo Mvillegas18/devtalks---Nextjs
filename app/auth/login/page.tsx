430cea356c50bcb81aa036902577f2a35db20bb4
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-03-28 14:27:08 -0500

    add LoginForm and RegisterForm components to login and register pages; update navigation links for consistency

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,8 @@
-import { Heading } from '@/src/shared/components/typography/Heading'
+import { LoginForm } from '@/src/features/auth/components/LoginForm'
+import { Heading } from '@/components/typography/Heading'
 import { generatePageTitle } from '@/src/shared/utils/metadata'
 import { Metadata } from 'next'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
 	title: generatePageTitle('Iniciar Sesión'),
@@ -10,6 +12,15 @@ export default function LoginPage() {
 	return (
 		<>
 			<Heading>Iniciar Sesión</Heading>
+
+			<LoginForm />
+
+			<nav className="mt-20 flex justify-between">
+				<Link href="/auth/register" className="font-bold">
+					Crear cuenta
+				</Link>
+				<Link href="/auth/forgot-password">Olvide mi contraseña</Link>
+			</nav>
 		</>
 	)
 }
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,6 +1,8 @@
+import { RegisterForm } from '@/src/features/auth/components/RegisterForm'
 import { Heading } from '@/src/shared/components/typography/Heading'
 import { generatePageTitle } from '@/src/shared/utils/metadata'
 import { Metadata } from 'next'
+import Link from 'next/dist/client/link.js'
 
 export const metadata: Metadata = {
 	title: generatePageTitle('Crear Cuenta'),
@@ -10,6 +12,15 @@ export default function RegisterPage() {
 	return (
 		<>
 			<Heading>Crear Cuenta</Heading>
+
+			<RegisterForm />
+
+			<nav className="mt-20 flex justify-between">
+				<Link href="/auth/login" className="font-bold">
+					Iniciar sesion
+				</Link>
+				<Link href="/auth/forgot-password">Olvide mi contraseña</Link>
+			</nav>
 		</>
 	)
 }
