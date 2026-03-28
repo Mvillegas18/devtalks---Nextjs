8f08446c9cf586a45adfac6c24bcc4aa06e20a07
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-03-28 18:37:42 -0500

    add FormError component export to forms index file

--- src/shared/components/forms/index.ts
+++ src/shared/components/forms/index.ts
@@ -2,5 +2,6 @@ import { FormInput } from './FormInput'
 import { FormLabel } from './FormLabel'
 import { FormSubmit } from './FormSubmit'
 import { Form } from './Form'
+import { FormError } from './FormError'
 
-export { FormInput, FormLabel, FormSubmit, Form }
+export { FormInput, FormLabel, FormSubmit, Form, FormError }
