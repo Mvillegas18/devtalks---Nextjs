0896926873a07cedf3b6dcddf9921bb8107c7373
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-03-27 19:41:40 -0500

    update logo dimensions in Logo component to match new SVG specifications

--- src/shared/components/ui/Logo.tsx
+++ src/shared/components/ui/Logo.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image.js'
 
 export function Logo() {
-	return <Image src="/logo.svg" alt="DevTalks Logo" width={400} height={200} />
+	return <Image src="/logo.svg" alt="DevTalks Logo" width={1200} height={400} />
 }
