d34b374ee7e05b6246095a9c1aa27f8bc1bad833
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-03-28 18:38:06 -0500

    refactor RegisterForm to integrate react-hook-form and zod for validation

--- src/features/auth/components/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm.tsx
@@ -1,44 +1,73 @@
 'use client'
-
+import { useForm } from 'react-hook-form'
 import {
 	Form,
+	FormError,
 	FormInput,
 	FormLabel,
 	FormSubmit,
 } from '@/components/forms/index'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { type SignUpInput, SignUpSchema } from '../shemas/authSchema'
+import { signUpAction } from '../actions/auth-actions'
 
 export function RegisterForm() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: zodResolver(SignUpSchema),
+		mode: 'all',
+	})
+
+	const onSubmit = async (data: SignUpInput) => {
+		await signUpAction(data)
+	}
+
 	return (
-		<Form>
-			<FormLabel htmlFor="nombre">Nombre:</FormLabel>
+		<Form onSubmit={handleSubmit(onSubmit)}>
+			<FormLabel htmlFor="name">Nombre:</FormLabel>
 			<FormInput
 				type="text"
-				id="nombre"
-				name="nombre"
+				id="name"
 				placeholder="Ingresa tu nombre..."
+				{...register('name')}
 			/>
+
+			{errors.name && <FormError>{errors.name.message}</FormError>}
+
 			<FormLabel htmlFor="email">E-mail:</FormLabel>
 			<FormInput
 				type="email"
 				id="email"
-				name="email"
 				placeholder="Ingresa tu E-mail..."
+				{...register('email')}
 			/>
+
+			{errors.email && <FormError>{errors.email.message}</FormError>}
 
 			<FormLabel htmlFor="password">Contraseña:</FormLabel>
 			<FormInput
 				type="password"
 				id="password"
-				name="password"
 				placeholder="Ingresa tu contraseña..."
+				{...register('password')}
 			/>
-			<FormLabel htmlFor="password_confirmation">Repetir contraseña:</FormLabel>
+
+			{errors.password && <FormError>{errors.password.message}</FormError>}
+
+			<FormLabel htmlFor="passwordConfirmation">Repetir contraseña:</FormLabel>
 			<FormInput
 				type="password"
-				id="password_confirmation"
-				name="password_confirmation"
+				id="passwordConfirmation"
 				placeholder="Repite tu contraseña..."
+				{...register('passwordConfirmation')}
 			/>
+
+			{errors.passwordConfirmation && (
+				<FormError>{errors.passwordConfirmation.message}</FormError>
+			)}
 
 			<FormSubmit value="Registrarme" />
 		</Form>
