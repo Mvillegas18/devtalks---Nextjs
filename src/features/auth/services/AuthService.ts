272e3b058e035772afa22b00a5c627815e6a5a27
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-04-04 16:25:54 -0500

    add: create database schema for users, sessions, accounts, and verifications

--- src/features/auth/services/AuthService.ts
+++ src/features/auth/services/AuthService.ts
@@ -1,14 +1,22 @@
+import { auth } from '@/src/lib/auth.js'
 import type { SignUpInput } from '../shemas/authSchema.js'
 
 class AuthService {
 	async register(credentials: SignUpInput) {
-		const {name, email, password} = credentials
+		const { name, email, password } = credentials
 
-        // Revisar si el usuario ya existe en la base de datos
+		// Revisar si el usuario ya existe en la base de datos
 
-        // Validacion de negocio
+		// Validacion de negocio
 
-        // Manejar el registro del usuario en la base de datos
+		// Manejar el registro del usuario en la base de datos
+		await auth.api.signUpEmail({
+			body: {
+				name,
+				email,
+				password,
+			},
+		})
 	}
 }
 
