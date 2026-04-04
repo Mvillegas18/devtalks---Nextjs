2caee4575e4b10083fb44ba928611d1ed09c6b51
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-04-04 17:32:40 -0500

    feat: implement userExist method in AuthRepository to check for existing users

--- src/features/auth/services/AuthRepository.ts
+++ src/features/auth/services/AuthRepository.ts
@@ -1,3 +1,15 @@
-class AuthRepository {}
+import { db } from '@/src/db/index'
+import { User } from '@/src/features/auth/types/auth'
+
+export interface IAuthRepository {
+	userExist(email: string): Promise<User | undefined>
+}
+class AuthRepository implements IAuthRepository {
+	async userExist(email: string) {
+		return await db.query.users.findFirst({
+			where: { email },
+		})
+	}
+}
 
 export const authRepository = new AuthRepository()
