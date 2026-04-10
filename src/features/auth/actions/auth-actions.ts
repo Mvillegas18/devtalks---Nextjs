b14a024bfe676520dc7ba5280c4fa0d51a40dd45
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-04-10 18:01:17 -0500

    feat: add signInAction for user authentication with validation

--- src/features/auth/actions/auth-actions.ts
+++ src/features/auth/actions/auth-actions.ts
@@ -1,7 +1,12 @@
 'use server'
 
 import { authService } from '../services/AuthService'
-import { SignUpSchema, type SignUpInput } from '../shemas/authSchema'
+import {
+	SignInInput,
+	SignInSchema,
+	SignUpSchema,
+	type SignUpInput,
+} from '../shemas/authSchema'
 
 export async function signUpAction(input: SignUpInput) {
 	const { success, data } = SignUpSchema.safeParse(input)
@@ -14,5 +19,18 @@ export async function signUpAction(input: SignUpInput) {
 	}
 
 	const response = await authService.register(data)
+	return response
+}
+
+export async function signInAction(input: SignInInput) {
+	const { success, data } = SignInSchema.safeParse(input)
+	if (!success) {
+		return {
+			error: 'Hubo un error',
+			success: '',
+		}
+	}
+
+	const response = await authService.login(data)
 	return response
 }
