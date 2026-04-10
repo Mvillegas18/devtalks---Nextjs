e738567b5272437cb378aeea6151e2c53c2388d2
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-04-10 18:13:09 -0500

    feat: enhance LoginForm with form validation and error handling

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,25 +1,58 @@
 'use client'
 
-import { Form, FormInput, FormLabel, FormSubmit } from '@/components/forms'
+import {
+	Form,
+	FormError,
+	FormInput,
+	FormLabel,
+	FormSubmit,
+} from '@/components/forms'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { type SignInInput, SignInSchema } from '../shemas/authSchema'
+import { signInAction } from '@/src/features/auth/actions/auth-actions'
+import { toast } from 'react-hot-toast/headless'
 
 export function LoginForm() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: zodResolver(SignInSchema),
+		mode: 'all',
+	})
+
+	const onSubmit = async (data: SignInInput) => {
+		const { error, success } = await signInAction(data)
+
+		if (error) {
+			toast.error(error)
+		}
+		if (success) {
+			toast.success(success)
+		}
+	}
+
 	return (
-		<Form>
+		<Form onSubmit={handleSubmit(onSubmit)}>
 			<FormLabel htmlFor="email">E-mail:</FormLabel>
 			<FormInput
 				type="email"
 				id="email"
-				name="email"
 				placeholder="Ingresa tu E-mail..."
+				{...register('email', { required: 'El correo es requerido' })}
 			/>
+			{errors.email && <FormError>{errors.email.message}</FormError>}
 
 			<FormLabel htmlFor="password">Password:</FormLabel>
 			<FormInput
 				type="password"
 				id="password"
-				name="password"
 				placeholder="Ingresa tu contraseña..."
+				{...register('password', { required: 'La contraseña es requerida' })}
 			/>
+			{errors.password && <FormError>{errors.password.message}</FormError>}
 
 			<FormSubmit value="Iniciar sesión" />
 		</Form>
