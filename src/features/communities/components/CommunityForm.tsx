83eb9ac6e895e0ba10e2b7253fcea79e37e7e71c
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-05-11 20:35:12 -0500

    feat: integrate react-hook-form for CommunityForm validation

--- src/features/communities/components/CommunityForm.tsx
+++ src/features/communities/components/CommunityForm.tsx
@@ -1,25 +1,35 @@
 import { FormInput } from '@/src/shared/components/forms/FormInput'
 import { FormLabel } from '@/src/shared/components/forms/FormLabel'
 import { FormTextArea } from '@/src/shared/components/forms/FormTextArea'
+import { useFormContext } from 'react-hook-form'
+import { type CommunityInput } from '../schema/communitySchema'
+import { FormError } from '@/src/shared/components/forms/FormError'
 
 export function CommunityForm() {
+	const {
+		register,
+		formState: { errors },
+	} = useFormContext<CommunityInput>()
 	return (
 		<>
 			<FormLabel htmlFor="name">Nombre de comunidad</FormLabel>
-			<FormInput id="name" name="name" placeholder="Nombre de comunidad" />
+			<FormInput
+				id="name"
+				placeholder="Nombre de comunidad"
+				{...register('name')}
+			/>
+			{errors.name && <FormError>{errors.name.message}</FormError>}
 
 			<FormLabel htmlFor="description">Descripción</FormLabel>
-			<FormInput
-				id="description"
-				name="description"
-				placeholder="Descripción de la comunidad"
-			/>
 
 			<FormTextArea
-				id="rules"
-				name="rules"
-				placeholder="Reglas de la comunidad"
+				id="description"
+				placeholder="Descripción de la comunidad"
+				{...register('description')}
 			/>
+			{errors.description && (
+				<FormError>{errors.description.message}</FormError>
+			)}
 		</>
 	)
 }
