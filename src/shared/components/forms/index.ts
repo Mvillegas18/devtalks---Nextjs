24f4f292a9d97584b95c186145c15dab2de202f6
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-05-11 19:58:41 -0500

    feat: export FormTextArea component in forms index

--- src/shared/components/forms/index.ts
+++ src/shared/components/forms/index.ts
@@ -3,5 +3,6 @@ import { FormLabel } from './FormLabel'
 import { FormSubmit } from './FormSubmit'
 import { Form } from './Form'
 import { FormError } from './FormError'
+import { FormTextArea } from './FormTextArea'
 
-export { FormInput, FormLabel, FormSubmit, Form, FormError }
+export { FormInput, FormLabel, FormSubmit, Form, FormError, FormTextArea }
