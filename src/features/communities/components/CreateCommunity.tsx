8ec0fb8a47c449249e4bbc76d9f053477939b543
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-05-11 20:35:32 -0500

    feat: implement form validation for CreateCommunity using zod and react-hook-form

--- src/features/communities/components/CreateCommunity.tsx
+++ src/features/communities/components/CreateCommunity.tsx
@@ -2,12 +2,31 @@
 import { Form } from '@/src/shared/components/forms/Form'
 import { FormSubmit } from '@/src/shared/components/forms/FormSubmit'
 import { CommunityForm } from './CommunityForm'
+import { FormProvider, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { CommunityInput, CommunitySchema } from '../schema/communitySchema'
+import { createCommunityAction } from '../actions/community-Action.js'
 
 export function CreateCommunity() {
+	const methods = useForm({
+		resolver: zodResolver(CommunitySchema),
+		mode: 'all',
+		defaultValues: {
+			name: '',
+			description: '',
+		},
+	})
+
+	const onSubmit = async (data: CommunityInput) => {
+		await createCommunityAction(data)
+	}
+
 	return (
-		<Form>
-			<CommunityForm />
-			<FormSubmit value="Crear Comunidad" />
-		</Form>
+		<FormProvider {...methods}>
+			<Form onSubmit={methods.handleSubmit(onSubmit)}>
+				<CommunityForm />
+				<FormSubmit value="Crear Comunidad" />
+			</Form>
+		</FormProvider>
 	)
 }
--- src/shared/components/forms/FormLabel.tsx
+++ src/shared/components/forms/FormLabel.tsx
@@ -5,7 +5,7 @@ type Props = LabelHTMLAttributes<HTMLLabelElement>
 export function FormLabel(props: Props) {
 	return (
 		<div>
-			<label className="block" {...props}>
+			<label className="block font-medium" {...props}>
 				{props.children}
 			</label>
 		</div>
