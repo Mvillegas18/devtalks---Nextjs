c9b37e9b713e8f2ed5b044424d7d63dbdb4a9cc8
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-05-12 20:09:59 -0500

    feat: add tailwind-merge dependency and update next.config for image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
 	/* config options here */
 	typedRoutes: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'pktgskxppz.ufs.sh',
+				// This pattern allows images from any path under imagedelivery.net
+			},
+		],
+	},
 }
 
 export default nextConfig
