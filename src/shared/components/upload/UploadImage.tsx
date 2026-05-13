f0414a96ce089a76149c80b4a7bb3e99e53a3efb
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-05-12 20:29:27 -0500

    feat: enhance image upload functionality with validation and error handling

--- src/shared/components/upload/UploadImage.tsx
+++ src/shared/components/upload/UploadImage.tsx
@@ -2,15 +2,27 @@ import { useState } from 'react'
 import { UploadDropzone } from '../../utils/uploadthing'
 import Image from 'next/image'
 import { twMerge } from 'tailwind-merge'
+import { useFormContext } from 'react-hook-form'
+import { CommunityInput } from '@/src/features/communities/schema/communitySchema'
+import { FormError } from '../forms/FormError'
 
 export function UploadImage() {
+	const {
+		formState: { errors },
+		setValue,
+		clearErrors,
+	} = useFormContext<CommunityInput>()
 	const [uploadedImage, setUploadedImage] = useState('')
 	return (
 		<>
 			<UploadDropzone
 				endpoint={'meetiUploader'}
 				className="ut-button:bg-orange-600 ut-label:font-bold ut-label:text-orange-600 ut-label:py-2 ut-label:px-4 ut-label:rounded after:ut-button:bg-orange-500 after:ut-button:h-2 after:ut-button:top-0 ut-button:w-full"
-				onClientUploadComplete={(res) => setUploadedImage(res[0].ufsUrl)}
+				onClientUploadComplete={(res) => {
+					setUploadedImage(res[0].ufsUrl)
+					setValue('image', res[0].ufsUrl)
+					clearErrors('image')
+				}}
 				config={{
 					cn: twMerge,
 					mode: 'auto',
@@ -21,6 +33,7 @@ export function UploadImage() {
 					allowedContent: 'Maximo 1 imagen de 1MB',
 				}}
 			/>
+			{errors.image && <FormError>{errors.image.message}</FormError>}
 
 			{uploadedImage && (
 				<div className="mt-4">
