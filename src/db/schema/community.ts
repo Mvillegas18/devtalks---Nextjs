b350b08c7ccf2b891a10178cf45b3b322ea26811
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-05-12 20:29:18 -0500

    feat: add image column to communities table and update schema

--- src/db/schema/community.ts
+++ src/db/schema/community.ts
@@ -4,6 +4,7 @@ export const community = pgTable('communities', {
 	id: uuid('id').primaryKey().defaultRandom(),
 	name: varchar('name', { length: 255 }).notNull(),
 	description: text('description').notNull(),
+	image: varchar('image', { length: 255 }).notNull(),
 	createdAt: timestamp('created_at').notNull().defaultNow(),
 	createdBy: text('created_by').notNull(),
 })
